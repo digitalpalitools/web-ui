a4dec12e711b7a841b6be180f6b05059f7abc1ca
smkvr98 <smkvr98@gmail.com>
2021-03-27 18:59:37 +0530

    feat: add locale json refresh script + data

---
+++ public/locales/refreshLocaleFiles.py
@@ -0,0 +1,22 @@
+import json
+import csv
+from collections import defaultdict
+
+def ndd():
+        return defaultdict(ndd)
+
+dictionaryOfLocales = ndd()
+
+with open("UI-localisation.csv") as localisationFile:
+    csvReader = csv.reader(localisationFile)
+    next(csvReader)
+    listOfLocales = next(csvReader)[1:]
+    for row in csvReader:
+        row[0] = row[0].split("__")
+        for index, entry in enumerate(row[1:]):
+            if entry:
+                dictionaryOfLocales[listOfLocales[index]][row[0][0]][row[0][1]] = entry
+        dictionaryOfLocales["en"]["WordFreq"]["WFSpace"] = " "
+    for locale in dictionaryOfLocales:
+        json.dump(dictionaryOfLocales[locale], open(
+            "translation.{}.json".format(locale), "w+"), indent=2, ensure_ascii=False)
