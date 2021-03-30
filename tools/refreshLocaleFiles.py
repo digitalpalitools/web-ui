9b9ad3d8c6b01e8b74e242f979971992245ebc1f
smkvr98 <smkvr98@gmail.com>
2021-03-30 17:02:09 +0530

    feature: make locale updation script verbose

--- tools/refreshLocaleFiles.py
+++ tools/refreshLocaleFiles.py
@@ -18,5 +18,10 @@ with open("UI-strings.csv") as localisationFile:
                 dictionaryOfLocales[listOfLocales[index]][row[0][0]][row[0][1]] = entry
         dictionaryOfLocales["en"]["WordFreq"]["WFSpace"] = " "
     for locale in dictionaryOfLocales:
+        print("---------------------------------------------------------")
+        print("Updating Locale {}".format(locale))
+        print(json.dumps(dictionaryOfLocales[locale], indent=2, ensure_ascii=False, sort_keys=True))
         json.dump(dictionaryOfLocales[locale], open(
             "./public/locales/translation.{}.json".format(locale), "w+"), indent=2, ensure_ascii=False, sort_keys=True)
+        print("---------------------------------------------------------")
+
